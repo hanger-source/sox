090700d45430c343e5567b57cf5c733b68fb0327
fuhangbo <fuhangbo@nnuo.com>
2022-10-21 15:37:36 +0800

    init

--- src/main/java/cn/uhfun/sox/SoxApplication.java
+++ src/main/java/cn/uhfun/sox/SoxApplication.java
@@ -3,11 +3,14 @@ package cn.uhfun.sox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author uhfun
+ */
 @SpringBootApplication
 public class SoxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoxApplication.class, args);
+    }
 
 }
