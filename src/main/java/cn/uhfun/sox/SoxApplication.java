36cbcef449cbdf22be48be5fa985e7ba486a281e
fuhangbo <fuhangbo@nnuo.com>
2022-10-21 15:33:52 +0800

    Initial commit

---
+++ src/main/java/cn/uhfun/sox/SoxApplication.java
@@ -0,0 +1,13 @@
+package cn.uhfun.sox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/uhfun/sox/SoxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.uhfun.sox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
